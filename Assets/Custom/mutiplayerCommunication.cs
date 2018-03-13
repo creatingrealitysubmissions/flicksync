0bb0cddf01eeded834f7b5e397950c3b573ac2e9
Nickymonkey <nickyroewe@mac.com>
2018-03-13 11:11:36 -0700

    forgot to push the script

---
+++ Assets/Custom/mutiplayerCommunication.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class mutiplayerCommunication : Photon.MonoBehaviour {
+
+	public WatsonIntegration currentScript;
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			// We own this player: send the others our data
+			stream.SendNext(currentScript.phraseNumber);
+		}
+		else
+		{
+			// Network player, receive data
+			this.currentScript.phraseNumber = (int)stream.ReceiveNext();
+		}
+	}
+}
