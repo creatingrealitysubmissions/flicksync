d1463a20ecf13e507622a42c7019ef1a102cb92b
dgeisert <memphis.gatech@gmail.com>
2018-03-13 10:00:31 -0700

    room named Alice instead of random

--- Assets/Photon Unity Networking/UtilityScripts/ConnectAndJoinRandom.cs
+++ Assets/Photon Unity Networking/UtilityScripts/ConnectAndJoinRandom.cs
@@ -41,19 +41,19 @@ public class ConnectAndJoinRandom : Photon.MonoBehaviour
     public virtual void OnConnectedToMaster()
     {
         Debug.Log("OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room. Calling: PhotonNetwork.JoinRandomRoom();");
-        PhotonNetwork.JoinRandomRoom();
+		PhotonNetwork.JoinRoom("Alice");
     }
 
     public virtual void OnJoinedLobby()
     {
         Debug.Log("OnJoinedLobby(). This client is connected and does get a room-list, which gets stored as PhotonNetwork.GetRoomList(). This script now calls: PhotonNetwork.JoinRandomRoom();");
-        PhotonNetwork.JoinRandomRoom();
+		PhotonNetwork.JoinRoom("Alice");
     }
 
     public virtual void OnPhotonRandomJoinFailed()
     {
         Debug.Log("OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one. Calling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
-        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 4 }, null);
+        PhotonNetwork.CreateRoom("Alice", new RoomOptions() { MaxPlayers = 4 }, null);
     }
 
     // the following methods are implemented to give you some context. re-implement them as needed.
