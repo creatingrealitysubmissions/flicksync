90c571257856e316025b7f619c40cfe9ae51f8eb
Nickymonkey <nickyroewe@mac.com>
2018-03-13 15:35:23 -0700

    more timer shtuff

--- Assets/Custom/mutiplayerCommunication.cs
+++ Assets/Custom/mutiplayerCommunication.cs
@@ -5,20 +5,20 @@ using Photon;
 
 public class mutiplayerCommunication : Photon.MonoBehaviour {
 
-	public int tempNumber;
-
-	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-	{
-		if (stream.isWriting)
-		{
-			// We own this player: send the others our data
-			stream.SendNext(WatsonIntegration.instance.phraseNumber);
-		}
-		else
-		{
-			// Network player, receive data
-			WatsonIntegration.instance.phraseNumber = (int)stream.ReceiveNext();
-		}
-		tempNumber = WatsonIntegration.instance.phraseNumber;
-	}
+//	public int tempNumber;
+//
+//	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+//	{
+//		if (stream.isWriting)
+//		{
+//			// We own this player: send the others our data
+//			stream.SendNext(WatsonIntegration.instance.phraseNumber);
+//		}
+//		else
+//		{
+//			// Network player, receive data
+//			WatsonIntegration.instance.phraseNumber = (int)stream.ReceiveNext();
+//		}
+//		tempNumber = WatsonIntegration.instance.phraseNumber;
+//	}
 }
