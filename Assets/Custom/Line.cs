90022f83ad4be84cf9b652306f4658157e14ce6b
dgeisert <memphis.gatech@gmail.com>
2018-03-12 23:17:39 -0700

    json reading working

---
+++ Assets/Custom/Line.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+[System.Serializable]
+public class Line {
+	public string character;
+	public string type;
+	public string[] description;
+	public string audioFile;
+	public bool is_npc;
+}
---
+++ Assets/Custom/LoadScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScript : MonoBehaviour {
+
+	ScriptConfig script;
+
+	// Use this for initialization
+	void Start () {
+		string json = "";
+		json = Resources.Load ("script").ToString();
+		Debug.Log (json);
+		script = JsonUtility.FromJson<ScriptConfig>(json);
+		Debug.Log (script.lines [0].description [0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
