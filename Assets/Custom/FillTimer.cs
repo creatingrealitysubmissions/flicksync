837f42b0afed3e89414bc46636fdcfe6784f9045
Cindy Reichel <cindy@cindydesmarais.com>
2018-03-13 18:39:11 -0700

    added visual timer

---
+++ Assets/Custom/FillTimer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillTimer : MonoBehaviour {
+
+	public Image fillImg;
+	public float timeAmt;
+	float time;
+	public bool isImgOn = false;
+
+	// Use this for initialization
+	void OnEnable () {
+		isImgOn = true;
+		fillImg = this.GetComponent<Image>();
+		fillImg.enabled = true;
+		time = timeAmt;
+		fillImg.fillAmount = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(time > 0) {
+			time -= Time.deltaTime;
+			fillImg.fillAmount = time /timeAmt; 
+		}
+
+		
+		if (Input.GetKeyDown("i")) {
+			if (isImgOn == true){
+				fillImg.color = Color.clear;
+				isImgOn = false;
+			}
+		}
+		else {
+			fillImg.color = Color.red;
+			isImgOn = true;
+		}
+	}
+}
+
