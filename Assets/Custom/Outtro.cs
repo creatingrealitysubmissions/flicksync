659cac589a7b3060316f189ca05da078d48ed00b
dgeisert <memphis.gatech@gmail.com>
2018-03-14 13:15:14 -0700

    outtro

---
+++ Assets/Custom/Outtro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outtro : MonoBehaviour {
+
+    public static Outtro instance;
+
+    public GameObject OuttroCanvas;
+    public UnityEngine.UI.Text percent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        instance = this;
+        OuttroCanvas.gameObject.SetActive(false);
+    }
+
+    public void End()
+    {
+        percent.text = Mathf.Round(WatsonIntegration.instance.percentageScore * 100).ToString() + "%";
+        OuttroCanvas.gameObject.SetActive(true);
+    }
+}
